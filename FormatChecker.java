dc0abed042408b8188d4dfad7cfde7555e04a769
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-09-06 16:27:20 -0600

    finish

--- FormatChecker.java
+++ FormatChecker.java
@@ -5,27 +5,43 @@ import java.io.FileNotFoundException;
 class FormatChecker {
     private int[][] matrix;
 
-    public FormatChecker (String filename) throws FileNotFoundException {
+    public FormatChecker(String filename) throws FileNotFoundException {
         matrix = readFile(filename);
     }
 
-    private int[][] readFile (String filename) throws FileNotFoundException {
+    private int[][] readFile(String filename) throws FileNotFoundException {
         File file = new File(filename);
         Scanner scnr = new Scanner(file);
 
-        int rowSize = scnr.nextInt();
-        int colSize = scnr.nextInt();
-
+        int rowSize = Integer.parseInt(scnr.next());
+        int colSize = Integer.parseInt(scnr.nextLine().trim());
 
         int[][] mat = new int[rowSize][colSize];
 
-            for (int row = 0; row < rowSize; row++) {
-                for (int col = 0; col < colSize; col++) {
+        for (int row = 0; row < rowSize; row++) {
+            for (int col = 0; col < colSize; col++) {
+                if (scnr.hasNextInt()) {
                     mat[row][col] = scnr.nextInt();
                 }
             }
-            
+        }
+
         scnr.close();
         return mat;
+    }
+
+    public boolean isValid() {
+        return true;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+
+        return str;
     }
 }
